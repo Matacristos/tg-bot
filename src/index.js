3b4cd68d51b91626554fdae349dd649b6b6746b3
Germán Castro <germancastromontes@gmail.com>
2020-10-11 14:30:47 +0200

    first setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+import { Telegraf } from 'telegraf'
+
+dotenv.config();
+
+
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.command('oldschool', (ctx) => ctx.reply('Hello'))
+bot.command('modern', ({ reply }) => reply('Yo'))
+bot.command('hipster', Telegraf.reply('λ'))
+bot.launch()
