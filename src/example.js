a010760d8d81ec3d986c38544c929f2483478c66
Germán Castro <germancastromontes@gmail.com>
2020-10-12 17:30:37 +0200

    updating command (testing heroku deploy)

--- src/example.js
+++ src/example.js
@@ -2,11 +2,14 @@ import axios from 'axios';
 
 const url = "https://api.coinranking.com/v1/public/coin"
 
-const getBitcoin = async () => {
-    const res = await axios.get(`${url}/1`);
+export const coin = {
+    BITCOIN: 1,
+}
+
+export default async (coin = coin.BITCOIN) => {
+    const res = await axios.get(`${url}/${coin}`);
     const { data } = res.data
     const { name, symbol, price } = data?.coin
     return `Current price of ${name}(${symbol}): ${price}`
 };
 
-export default getBitcoin;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import dotenv from 'dotenv';
 import { Telegraf } from 'telegraf'
-import getCoin from './example';
+import getCoin, { coin } from './example';
 
 dotenv.config();
 
 
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.command('quote', async ({ reply }) => {
-    const msg = await getCoin();
+bot.command('bitcoin', async ({ reply }) => {
+    const msg = await getCoin(coin.BITCOIN);
     return reply(msg)
 })
 bot.launch()
