8a3b7e1e1122af77c6507c8e0c385b02dd183069
Navega <navega@appfluence.com>
2020-10-19 15:43:44 +0200

    Some basic utilities (catch & help)

--- src/index.js
+++ src/index.js
@@ -5,10 +5,19 @@ import getCoin, { coin } from './example';
 dotenv.config();
 
 
-
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.command('bitcoin', async ({ reply }) => {
     const msg = await getCoin(coin.BITCOIN);
     return reply(msg)
 })
+
+bot.command(['start', 'help'], async (ctx) => {
+    const text = `Try next commands.\n\n/help — list commands\n/bitcoin — get information about bitcoins from our AI`
+    ctx.replyWithMarkdown(text)
+})
+
+bot.catch((err, ctx) => {
+    console.log(`Ooops, encountered an error for ${ctx.updateType}`, err)
+})
+
 bot.launch()
