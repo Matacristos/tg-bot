6deed698a9ce9cc70bdef0a58e1572415b8cfd0f
davidlorca <lorcacamposdavid@gmail.com>
2020-10-24 11:14:05 +0200

    Command to send images to chat added

--- src/index.js
+++ src/index.js
@@ -2,17 +2,25 @@ import dotenv from 'dotenv';
 import { Telegraf } from 'telegraf'
 import getCoin, { coin } from './example';
 
+
 dotenv.config();
 
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
+const graphURL = 'https://www.solvetic.com/uploads/articles/monthly_11_2016/12dbd9b70a81765943eeb6bf6cb68e3c.png' //Provided by API (Example given)
+
+
 bot.command('bitcoin', async ({ reply }) => {
     const msg = await getCoin(coin.BITCOIN);
     return reply(msg)
 })
 
+bot.command('getGraph', async (ctx) => { //Function that sends pictures (graphics in this case) to chat
+    ctx.replyWithPhoto(graphURL)
+})
+
 bot.command(['start', 'help'], async (ctx) => {
-    const text = `Try next commands.\n\n/help — list commands\n/bitcoin — get information about bitcoins from our AI`
+    const text = `Try next commands.\n\n/help — list commands\n/bitcoin — get information about bitcoins from our AI\n/getGraph — get historical information about bitcoin`
     ctx.replyWithMarkdown(text)
 })
 
