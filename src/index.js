a14b5e43a03e8e3a8c3816ebf8bb57a46c514b3c
Navega <navega@appfluence.com>
2020-10-26 11:37:15 +0100

    Some hardcoded jokes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
 import { Telegraf } from 'telegraf'
+const Markup = require('telegraf/markup')
+const Extra = require('telegraf/extra')
 import getCoin, { coin } from './example';
 import connect, { getGraphUrl } from './utils'
 
@@ -33,6 +35,37 @@ bot.command(['start', 'help'], async (ctx) => {
 
 bot.catch((err, ctx) => {
     console.log(`Ooops, encountered an error for ${ctx.updateType}`, err)
+})
+
+// Hardcoded jokes
+
+bot.hears('soy angel', (ctx) => ctx.replyWithPhoto('https://cdn.memegenerator.es/imagenes/memes/full/31/71/31710992.jpg'))
+
+bot.hears('hola', (ctx) => {
+    ctx.reply('<b>Hola</b>. <i>Quién eres?</i>',
+        Extra.HTML()
+            .markup(Markup.inlineKeyboard([
+                Markup.callbackButton('David', 'David'),
+                Markup.callbackButton('German', 'German'),
+                Markup.callbackButton('Paolo', 'Paolo'),
+                Markup.callbackButton('Navega', 'Navega')
+            ])))
+})
+bot.action('David', (ctx) => {
+    ctx.editMessageText('<b>Hola David, lo siento pero esto no es Tinder 🍆</b>',
+        Extra.HTML())
+})
+bot.action('German', (ctx) => {
+    ctx.editMessageText('Hola Germán... Te recomiendo este enlace <b>https://1.ooskar.com/ </b>',
+        Extra.HTML())
+})
+bot.action('Paolo', (ctx) => {
+    ctx.editMessageText('<b>Hola Paolo, en qué local de copas te gustaría invertir hoy? 🙏</b>',
+        Extra.HTML())
+})
+bot.action('Navega', (ctx) => {
+    ctx.editMessageText('<b>Hola Navega, estoy aquí para servirte 😊</b>',
+        Extra.HTML())
 })
 
 bot.launch()
